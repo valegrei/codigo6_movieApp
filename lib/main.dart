c309315097e09e725f74d434469996a558579508
valegrei <victoralegre@uni.pe>
2023-05-02 17:32:46 -0500

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:codigo6_movieapp/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,8 +14,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.dmSansTextTheme(),
       ),
       home: const HomePage(),
     );
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,25 +1,104 @@
+import 'dart:convert';
+
+import 'package:codigo6_movieapp/models/movie_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
-  getData() async {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<MovieModel> moviesModel = [];
+
+  getDataInternet() async {
     Uri url = Uri.parse(
         "https://api.themoviedb.org/3/discover/movie?api_key=668dfd6ba68b6a7595f5d83a2a683550&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate");
     http.Response response = await http.get(url);
-    print(response);
-    print(response.statusCode);
-    print(response.body);
+    Map data = json.decode(response.body);
+    List movies = data["results"];
+    moviesModel = movies.map((e) => MovieModel.fromJson(e)).toList();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getDataInternet();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: const Text("GET DATA"),
-          onPressed: () {},
+      backgroundColor: const Color(0xff111111),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: GridView.builder(
+            itemCount: moviesModel.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 12.0,
+              crossAxisSpacing: 12.0,
+              childAspectRatio: 0.7,
+            ),
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12.0),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                          "https://image.tmdb.org/t/p/w500${moviesModel[index].posterPath}"),
+                    )),
+                child: Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12.0),
+                        gradient: LinearGradient(
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                            colors: [
+                              const Color(0xff111111).withOpacity(0.99),
+                              Colors.transparent,
+                              Colors.transparent,
+                            ]),
+                      ),
+                    ),
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              moviesModel[index].originalTitle,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Text(
+                              "(${moviesModel[index].releaseDate.year})",
+                              style: const TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
